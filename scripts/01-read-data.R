e258516ba04f4326c84a91cda0d05fb38dba7b27
rene <50000390+renedarioherrera@users.noreply.github.com>
2022-05-04 01:24:13 +0000

    updates

--- scripts/01-read-data.R
+++ scripts/01-read-data.R
@@ -34,7 +34,16 @@ poverty <- get_acs(
     "population" = "S1701_C01_001",
     "poverty" = "S1701_C03_002",
     "median_income" = "S1901_C01_012",
-    "median_rent" = "DP04_0134"),
+    "median_rent" = "DP04_0134",
+    "median_age" = "B01002_001",
+    "bachelor_degree_percent" = "DP02_0065P",
+    "broadband_percent" = "DP02_0153P",
+    "hispanic_percent" = "DP05_0071P",
+    "sex_percent" = "S0101_C06_008",
+    "womens_earnings_percent" = "S2411_C04_001",
+    "gini_index" = "B19083_001",
+    "white_not_hispanic" = "B01001H_001"
+    ),
   year = 2019,
   output = "tidy",
   geometry = FALSE,
@@ -49,6 +58,60 @@ poverty[c("tract", "county", "state")] <- str_split_fixed(poverty$name, pattern 
 
 # 
 poverty %>%
-  filter(estimate != 0) %>%
-  group_by(state) %>%
-  slice_min(n = 5, order_by = estimate)
+  drop_na(estimate) %>%
+  select(!(moe)) %>%
+  filter(variable == "sex_percent" | variable == "hispanic_percent") %>%
+  pivot_wider(
+    names_from = variable, values_from = c(estimate)
+  ) %>%
+  ggplot() +
+  geom_point(mapping= aes(x = hispanic_percent, y = sex_percent)) +
+  facet_wrap(~state)
+
+# 
+poverty %>%
+  drop_na(estimate) %>%
+  select(!(moe)) %>%
+  filter(variable == "median_rent" | variable == "median_income") %>%
+  pivot_wider(
+    names_from = variable, values_from = c(estimate)
+  ) %>%
+  ggplot() +
+  geom_point(mapping= aes(x = median_rent, y = median_income)) +
+  facet_wrap(~state)
+
+# 
+poverty %>%
+  drop_na(estimate) %>%
+  select(!(moe)) %>%
+  filter(variable == "gini_index" | variable == "median_income") %>%
+  pivot_wider(
+    names_from = variable, values_from = c(estimate)
+  ) %>%
+  ggplot() +
+  geom_point(mapping= aes(x = gini_index, y = median_income)) +
+  facet_wrap(~state)
+
+#
+poverty %>%
+  drop_na(estimate) %>%
+  select(!(moe)) %>%
+  filter(variable == "population" | variable == "poverty") %>%
+  pivot_wider(
+    names_from = variable, values_from = c(estimate)
+  ) %>%
+  ggplot() +
+  geom_point(mapping= aes(x = population, y = poverty)) +
+  facet_wrap(~state)
+
+#
+poverty %>%
+  drop_na(estimate) %>%
+  select(!(moe)) %>%
+  filter(variable == "womens_earnings_percent" | variable == "sex_percent") %>%
+  pivot_wider(
+    names_from = variable, values_from = c(estimate)
+  ) %>%
+  ggplot() +
+  geom_point(mapping= aes(x = womens_earnings_percent, y = sex_percent)) +
+  facet_wrap(~state)
